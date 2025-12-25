754402ffab7f9f3bdd81cb1b2d0f8d217bf3c3d7
eaglementality <jesseowusuyeboah99@gmail.com>
2025-12-25 20:15:03 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const interface = readline.createInterface({
+    input: 'process.stdin',
+    output: 'process.stdout'
+})
+
+interface.question('')
